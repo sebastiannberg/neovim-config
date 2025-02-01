c961c3deeffdee2550eb7d4b7eb165fc07c248ae
Sebastian August Berg <sebastian-august@hotmail.com>
2025-02-01 16:56:18 +0100

    Colorscheme

--- init.lua
+++ init.lua
@@ -10,4 +10,21 @@ require('core.options')
 require('core.keymaps')
 require('core.autocommands')
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Initialize lazy.nvim
+require('lazy').setup('plugins')
+
 -- vim: ts=2 sts=2 sw=2 et
