559de2560bd42c78c526e1565e5bb8d9cd5b5d87
Sebastian August Berg <sebastian-august@hotmail.com>
2025-02-02 23:41:54 +0100

    osc52 clipboard

--- init.lua
+++ init.lua
@@ -5,6 +5,19 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Set clipboard to OSC 52 to enable clipboard sync with OS over SSH
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 -- Load core settings
 require('core.options')
 require('core.keymaps')
