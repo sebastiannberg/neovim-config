0b455ca864c566b3193fc5458a57b363e93c8308
Sebastian August Berg <sebastian-august@hotmail.com>
2025-03-07 00:57:03 +0100

    more lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,6 +32,10 @@ return {
         -- LSP keymaps
         local opts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
       end
 
