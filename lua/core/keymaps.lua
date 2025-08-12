463c4900f63acecf0404d02c5e41576ed0bae1d7
Sebastian August Berg <sebastian-august@hotmail.com>
2025-08-12 16:51:44 +0200

    add exit terminal keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,10 @@ vim.keymap.set('n', '<leader>st', function()
   vim.cmd 'startinsert'
 end, { desc = 'Open a small terminal' })
 
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- NOTE: This won't work in all terminal emulators/tmux/etc.
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- Move lines up or down
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down', silent = true })
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up', silent = true })
@@ -22,4 +26,3 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
