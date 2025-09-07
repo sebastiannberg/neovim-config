1711fae4d50a7b6e372859af2ee94d19679f9380
Sebastian August Berg <sebastian-august@hotmail.com>
2025-09-07 16:48:07 +0200

    better statusline

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = 'auto',
+        section_separators   = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    },
+  },
+}
