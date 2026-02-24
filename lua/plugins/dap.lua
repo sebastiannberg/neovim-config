3d48cd5eb3e21553471f299017c0abb9ac27b1b5
Sebastian August Berg <sebastian-august@hotmail.com>
2026-02-24 09:28:38 +0100

    configure debug adapter

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,3 +1,31 @@
 return {
-  'mfussenegger/nvim-dap',
+  "mfussenegger/nvim-dap",
+  keys = {
+    { "<leader>dc", function() require("dap").continue() end, desc = "DAP Continue" },
+    { "<leader>do", function() require("dap").step_over() end, desc = "DAP Step Over" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "DAP Step Into" },
+    { "<leader>du", function() require("dap").step_out() end, desc = "DAP Step Out" },
+    { "<leader>b", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>B", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional Breakpoint" },
+    { "<leader>dR", function() require("dap").repl.open() end, desc = "DAP REPL" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "DAP Run Last" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "DAP Terminate" },
+  },
+  config = function()
+    local dap = require("dap")
+
+    dap.configurations.java = {
+      {
+        type = "java",
+        request = "attach",
+        name = "Attach to Remote JVM",
+        hostName = function()
+          return vim.fn.input("Host [127.0.0.1]: ", "127.0.0.1")
+        end,
+        port = function()
+          return tonumber(vim.fn.input("Port [5005]: ", "5005"))
+        end,
+      },
+    }
+  end,
 }
