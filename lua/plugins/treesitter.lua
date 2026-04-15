1856fb1ae550645567e00b284c18ad04c61edaa6
Sebastian August Berg <sebastian-august@hotmail.com>
2026-04-15 11:46:59 +0200

    New api in nvim 12 for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,41 +1,39 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'css',
-        'dockerfile',
-        'gitignore',
-        'go',
-        'html',
-        'java',
-        'javascript',
-        'json',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'matlab',
-        'python',
-        'query',
-        'ruby',
-        'rust',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end
+  opts = {
+    ensure_installed = {
+      'bash',
+      'c',
+      'cpp',
+      'css',
+      'dockerfile',
+      'gitignore',
+      'go',
+      'html',
+      'java',
+      'javascript',
+      'json',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'matlab',
+      'python',
+      'query',
+      'ruby',
+      'rust',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false
+    },
+    indent = {
+      enable = true,
+    },
+  },
 }
